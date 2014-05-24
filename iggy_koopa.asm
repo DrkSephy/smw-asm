79f8a3640cd2f4366eeb69d788ecae4d967b802c
David Leonard <sephirothcloud1025@yahoo.com>
2014-05-23 22:22:28 -0400

    Initializating properties

--- iggy_koopa.asm
+++ iggy_koopa.asm
@@ -10,3 +10,18 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+    SPRITE_X_SPEED = $B6
+    SPRITE_Y_SPEED = $AA
+    SPRITE_Y_POS   = $D8
+    ORIG_Y_POS     = $151C
+    MAX_Y_SPEED    = $3E
+    EXTRA_BITS     = $7FAB10
+    EXTRA_PROP_1   = $7FAB28
+    HIT_POINTS     = $05        ;  being stomped by Mario
+    H_OFFSCREEN    = $15A0
+    V_OFFSCREEN    = $186C
+    FREEZE_TIMER   = $1540
+    SPR_OBJ_STATUS = $1588
+    SMASH_STATUS   = $1602
+
+    
