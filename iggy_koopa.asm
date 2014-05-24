53260d7229fb095ceaad724b685053313a09b0a9
David Leonard <sephirothcloud1025@yahoo.com>
2014-05-23 22:19:41 -0400

    Initial commit for Iggy Koopa

---
+++ iggy_koopa.asm
@@ -0,0 +1,12 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;                    David Leonard                       ;
+;                Description: Iggy Koopa                 ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Uses first extra bit: NO
+; Extra Property Byte 1
+;   - bit 0: Enable Spin Killing
+;   - bit 1: stay on ledges
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
